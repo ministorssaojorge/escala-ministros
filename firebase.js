4ed8b038a57200cb50640f9a5f55fd8d0bab69ff
ministorssaojorge <ministorssaojorge@gmail.com>
2026-04-04 12:53:09 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
+  apiKey: "AIzaSyCqVWu40frpSsA92X1MFW6l6AvNE7N6HoA",
+  authDomain: "escala-de-ministros-d41e5.firebaseapp.com",
+  projectId: "escala-de-ministros-d41e5",
 };
 
 const app = initializeApp(firebaseConfig);
